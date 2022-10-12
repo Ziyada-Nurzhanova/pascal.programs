58aa0c5354a2efb838227ff3f1fd79048daf410f
Alice <aliceerror380@gmail.com>
2022-10-12 18:31:55 +0600

    Third_Differences

--- pascal/summa.pas
+++ pascal/summa.pas
@@ -1,11 +1,11 @@
 ﻿program Kraises;
-var x,y,sum,a:real;
+var x,y,sum,a:real;{this is variables;}
 begin
   x:=0;
   y:=x*x*x;
   sum:=x+y;
-  writeln(sum+1);
-  readln(a);
-  write(a+sum+1);
+  writeln(sum+1);{print}
+  readln(a);{input}
+  write(a+sum+1);{print}
   
 end.
